79bc196adeb6ebc93873e6a05ed53f0f553b1ebe
smithm <martin.smith@dowjones.com>
2017-05-31 11:18:21 +0100

    Refctor App to work with local storage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,60 +1,11 @@
 var React = require('react');
 
-var uuid = require('node-uuid');
-var moment = require('moment');
-
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-  getInitialState: function () {
-    return {
-      todos: TodoAPI.getTodos(),
-      showCompleted: false,
-      searchText: ''
-    };
-  },
-  componentDidUpdate: function () {
-    TodoAPI.setTodos(this.state.todos);
-  },
-  handleAddTodo: function (text) {
-    this.setState({
-      todos: [
-        ...this.state.todos,
-        {
-          id: uuid(),
-          text: text,
-          completed: false,
-          createdAt: moment().unix(),
-          completedAt: undefined
-        }
-      ]
-    })
-  },
-  handleSearch: function (showCompleted, searchText) {
-    this.setState({
-      showCompleted: showCompleted,
-      searchText: searchText.toLowerCase()
-    });
-  },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-      return todo;
-    });
-
-    this.setState({
-      todos: updatedTodos
-    });
-  },
   render: function () {
-    var {todos, showCompleted, searchText} = this.state;
-    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
     return (
       <div>
         <h1 className="page-title">Todo App</h1>
@@ -62,9 +13,9 @@ var TodoApp = React.createClass({
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
-              <TodoSearch onSearch={this.handleSearch}/>
-              <TodoList />
-              <AddTodo onAddTodo={this.handleAddTodo}/>
+              <TodoSearch/>
+              <TodoList/>
+              <AddTodo/>
             </div>
           </div>
         </div>
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -66,5 +66,26 @@ describe('Reducers', () => {
       expect(res[0].completedAt).toBeA('number');
 
     });
+
+    it ('should add exisiting todos', () => {
+      var todos = [
+      {
+        id: 111,
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }
+      ];
+
+      var action = {
+        type: 'ADD_TODOS',
+        todos
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length) .toEqual(1);
+      expect(res[0]).toEqual(todos[0])
+    });
   });
 });
