edaea1a02ea03964156d2c3212567b38d4f9e1f0
smithm <martin.smith@dowjones.com>
2017-05-31 08:51:15 +0100

    Add Todo reducers

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -31,4 +31,40 @@ describe('Reducers', () => {
       expect(res).toEqual(false);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle completed', () => {
+      var todos = [
+        {
+          id: '123',
+          text: 'Walk the dog',
+          completed: false,
+          completedAt: undefined,
+          createdAt: 123
+        }
+      ];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toBeA('number');
+
+    });
+  });
 });
